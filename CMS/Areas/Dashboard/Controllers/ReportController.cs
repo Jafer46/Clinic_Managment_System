7834c6f6771703b4e0e489a9be9a0ccebd9e3835
Jafer46 <125634413+Jafer46@users.noreply.github.com>
2024-01-07 03:07:06 -0800

    Add files via upload

---
+++ CMS/Areas/Dashboard/Controllers/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CMS.Data;
+
+namespace CMS.Areas.Dashboard.Controllers
+{
+    public class ReportController : Controller
+    {
+        
+        private readonly ApplicationDbContext _context;
+        public ReportController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [Area("Dashboard")]
+        public IActionResult Index()
+        {
+            var allEntries = _context.Sessions.Count();
+            var allDocters = _context.Users.Where(u => u.Role.Name.ToLower() == "docter");
+            ViewData["AllEntries"] = allEntries;
+            ViewData["Docter"] = allDocters;
+            return View();
+        }
+    }
+}
