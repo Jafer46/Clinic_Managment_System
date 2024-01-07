aeb226e8b81cc0cf8f18e5e3c05c6404842b62d1
Jafer46 <125634413+Jafer46@users.noreply.github.com>
2024-01-07 03:03:17 -0800

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CMS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
